2fd7d7675710bf790b04cca5f71410e1f1dde5bb
LePhuocDat <hsddung92.lpdat@gmail.com>
2021-09-30 14:46:03 +0700

    .'

--- Ubuntu/setup.sh
+++ Ubuntu/setup.sh
@@ -2,22 +2,30 @@
  
 sudo apt update -y 
 sudo apt install -y curl vlc zsh git htop tmux python3-pip
+sudo apt install -y tlp powertop
+sudo apt install -y nautilus
+
+sudo add-apt-repository ppa:linuxuprising/apps
+sudo apt update
+sudo apt install tlpui
+
+
 
 # sudo apt-add-repository universe
-sudo apt install gnome-tweak-tool
+# sudo apt install gnome-tweak-tool
 
 # sudo snap install --classic code
 # sudo apt install sublime-text --classic
 
-sudo apt install gnome-shell-extension-autohidetopbar
+# sudo apt install gnome-shell-extension-autohidetopbar
 
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 
-chsh -s $(which zsh)
+# chsh -s $(which zsh)
 
 
-gsettings set org.gnome.desktop.interface show-battery-percentage true
+# gsettings set org.gnome.desktop.interface show-battery-percentage true
