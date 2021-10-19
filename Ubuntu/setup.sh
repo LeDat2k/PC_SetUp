7a33a24d46d00ac7ed60013a9bcd481bd81d6785
LePhuocDat <55519714+LeDat2k@users.noreply.github.com>
2021-10-19 20:33:16 +0700

    change...

--- Ubuntu/setup.sh
+++ Ubuntu/setup.sh
@@ -1,31 +1,54 @@
 #!/bin/bash
  
 sudo apt update -y 
-sudo apt install -y curl vlc zsh git htop tmux python3-pip
-sudo apt install -y tlp powertop
-sudo apt install -y nautilus
+# terminal
+sudo apt install -y curl git rofi zsh htop vlc nautilus tmux python3-pip xclip
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 
+chsh -s $(which zsh)
+
+# sublime text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+# google chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# vscode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
+
+# power manager
+sudo apt install -y tlp powertop
 sudo add-apt-repository ppa:linuxuprising/apps
 sudo apt update
 sudo apt install tlpui
 
+# table plus
+wget -qO - http://deb.tableplus.com/apt.tableplus.com.gpg.key | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://deb.tableplus.com/debian tableplus main"
+sudo apt update
+sudo apt install tableplus
+
+# wps
+firefox https://linux.wps.com/
 
 
 # sudo apt-add-repository universe
 # sudo apt install gnome-tweak-tool
 
-# sudo snap install --classic code
-# sudo apt install sublime-text --classic
-
 # sudo apt install gnome-shell-extension-autohidetopbar
 
 
-# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
-
-# chsh -s $(which zsh)
-
-
-# gsettings set org.gnome.desktop.interface show-battery-percentage true
